3d7e3e823eb79dc794db81c05f94b6e5db824afe
s3723282 <s3723282@student.rmit.edu.au>
2022-08-12 11:39:38 +1000

    Adding project to github

---
+++ Procedural Generation/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    bool probabilityOfSpawn => Random.Range(0, 100) >= 32 && Random.Range(0, 100) <= 45;
+
+    void Start()
+    {
+        if (this.transform.position.y <= 2.0f &&
+            this.transform.position.y >= 1.5f &&
+            probabilityOfSpawn)
+        {
+            Instantiate(
+                objectToSpawn, new Vector3(
+                    this.transform.position.x,
+                    this.transform.position.y + 1.5f,
+                    this.transform.position.z
+                    ), Quaternion.identity
+                );
+        }
+           
+             if(probabilityOfSpawn) {
+                 Instantiate(objectToSpawn, new Vector3(
+                        this.transform.position.x, 
+                        this.transform.position.y, 
+                        this.transform.position.z
+                   ), Quaternion.identity
+                );
+            }   
+        }
+    }
